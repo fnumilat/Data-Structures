1e42df182e0876d49482699e3aa3cc7609dfb2ee
unknown <fnumilat@gmail.com>
2020-06-25 20:31:30 -0500

    strech done on binary_search_tree

--- stack/stack.py
+++ stack/stack.py
@@ -10,9 +10,7 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-import sys
 
-sys.path.append('../singly_linked_list')
 from singly_linked_list import LinkedList
 
 
